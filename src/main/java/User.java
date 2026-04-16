27c2ac573bfa2a90a1ea9b9ffe2f076abdeb1910
Chocoding1 <galmeagi2@naver.com>
2026-04-16 13:14:21 +0900

    User: 이름이 공백일 경우 예외 발생 코드 추가

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,6 +3,13 @@ public class User {
     private final String name;
 
     public User(String name) {
+        validateEmpty(name);
         this.name = name;
+    }
+
+    private void validateEmpty(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 사용자의 이름은 공백일 수 없습니다.");
+        }
     }
 }
