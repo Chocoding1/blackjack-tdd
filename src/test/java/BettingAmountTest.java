d2248d1a5db44c05246fe0da38537088967de729
Chocoding1 <galmeagi2@naver.com>
2026-04-16 19:36:26 +0900

    test(BettingAmountTest): 배팅 금액 숫자 타입 검증 테스트 작성

--- src/test/java/BettingAmountTest.java
+++ src/test/java/BettingAmountTest.java
@@ -3,15 +3,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class BettingAmountTest {
 
     @ParameterizedTest
-    @DisplayName("입력값이 공백일 경우 예외 발생")
+    @DisplayName("배팅 금액이 공백일 경우 예외 발생")
     @ValueSource(strings = {"", " "})
-    void createBettingAmount_fail_when_empty_input(String emptyInput) {
+    void createBettingAmount_fail_when_empty_amount(String emptyAmount) {
         // when & then
-        Assertions.assertThatThrownBy(() -> new BettingAmount(emptyInput))
+        assertThatThrownBy(() -> new BettingAmount(emptyAmount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 배팅 금액은 공백일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("배팅 금액이 숫자 타입이 아닐 경우 예외 발생")
+    @ValueSource(strings = {"string", "!@#$"})
+    void createBettingAmount_fail_when_not_integer_amount(String invalidAmount) {
+        // when & then
+        assertThatThrownBy(() -> new BettingAmount(invalidAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 배팅 금액은 숫자 형태로 입력해야 합니다.");
     }
 }
