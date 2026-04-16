6c37dde3a570a4e5d106c4bed13f0babdb12156a
Chocoding1 <galmeagi2@naver.com>
2026-04-16 19:50:54 +0900

    feat(BettingAmount): 배팅 금액 0 이하 검증 로직 작성

--- src/main/java/BettingAmount.java
+++ src/main/java/BettingAmount.java
@@ -5,6 +5,7 @@ public class BettingAmount {
     public BettingAmount(String input) {
         validateEmpty(input);
         int amount = convertToInt(input);
+        validateRange(amount);
         this.amount = amount;
     }
 
@@ -19,6 +20,12 @@ public class BettingAmount {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 배팅 금액은 숫자 형태로 입력해야 합니다.");
+        }
+    }
+
+    private void validateRange(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 0보다 커야 합니다.");
         }
     }
 }
