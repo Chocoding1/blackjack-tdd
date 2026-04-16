92fd74abdff8584331bd0a3fdb092948f02c28e9
Chocoding1 <galmeagi2@naver.com>
2026-04-16 18:31:26 +0900

    test(UserTest): 사용자의 이름에 특수문자, 공백, 숫자 검증 로직 추가

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class UserTest {
 
     @ParameterizedTest
@@ -10,8 +12,18 @@ public class UserTest {
     @ValueSource(strings = {"", " "})
     void createUser_fail_when_empty_name(String emptyName) {
         // when & then
-        Assertions.assertThatThrownBy(() -> new User(emptyName))
+        assertThatThrownBy(() -> new User(emptyName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 사용자의 이름은 공백일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("사용자의 이름에 특수문자가 존재할 경우 예외 발생")
+    @ValueSource(strings = {"pobi*", "po bi", "pobi123"})
+    void createUser_fail_when_name_contains_invalid_string(String invalidName) {
+        // when & then
+        assertThatThrownBy(() -> new User(invalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 사용자의 이름은 영어 또는 한글로만 입력할 수 있습니다.(띄어쓰기 불가)");
     }
 }
