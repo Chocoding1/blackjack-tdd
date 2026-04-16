54f53353a55c6abe3f6c916ae601192922660e12
Chocoding1 <galmeagi2@naver.com>
2026-04-16 13:37:00 +0900

    CommaSplitter: 입력값이 공백일 경우 예외 발생 테스트 작성

--- src/test/java/CommaSplitterTest.java
+++ src/test/java/CommaSplitterTest.java
@@ -1,4 +1,17 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class CommaSplitterTest {
 
-
+    @ParameterizedTest
+    @DisplayName("입력값이 공백일 경우 예외 발생")
+    @ValueSource(strings = {"", " "})
+    void nameSplit_fail_when_empty_input(String input) {
+        // when & then
+        Assertions.assertThatThrownBy(() -> CommaSplitter.split(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력값은 공백일 수 없습니다.");
+    }
 }
