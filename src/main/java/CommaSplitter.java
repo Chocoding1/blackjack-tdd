02513eead13015415ffa24082039608163a5b78b
Chocoding1 <galmeagi2@naver.com>
2026-04-16 13:48:00 +0900

    CommaSplitter: 입력값이 공백일 경우 예외를 발생시키는 로직 추가

--- src/main/java/CommaSplitter.java
+++ src/main/java/CommaSplitter.java
@@ -1,6 +1,12 @@
 public class CommaSplitter {
 
     public static void split(String input) {
+        validateEmptyInput(input);
+    }
 
+    private static void validateEmptyInput(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 입력값은 공백일 수 없습니다.");
+        }
     }
 }
