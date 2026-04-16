bb899bd23390c7b63865138750d6cf4b61da08d1
Chocoding1 <galmeagi2@naver.com>
2026-04-16 19:27:29 +0900

    feat(BettinAmount): 배팅 금액 공백 검증 로직 생성

--- src/main/java/BettingAmount.java
+++ src/main/java/BettingAmount.java
@@ -3,6 +3,13 @@ public class BettingAmount {
     private final String amount;
 
     public BettingAmount(String amount) {
+        validateEmpty(amount);
         this.amount = amount;
+    }
+
+    private void validateEmpty(String amount) {
+        if (amount.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 공백일 수 없습니다.");
+        }
     }
 }
