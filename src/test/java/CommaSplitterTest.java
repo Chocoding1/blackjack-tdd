b79eca353a7d4a517e545f2a21b4d8126126e099
Chocoding1 <galmeagi2@naver.com>
2026-04-16 15:44:24 +0900

    CommaSplitterTest: 쉼표로 구분한 후 List 반환 검증 테스트 코드 작성

--- src/test/java/CommaSplitterTest.java
+++ src/test/java/CommaSplitterTest.java
@@ -1,7 +1,12 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CommaSplitterTest {
 
@@ -13,5 +18,21 @@ public class CommaSplitterTest {
         Assertions.assertThatThrownBy(() -> CommaSplitter.split(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력값은 공백일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("올바른 형식의 입력값일 경우 쉼표로 구분하고, 구분한 문자열들의 개수가 옳아야 한다.")
+    @CsvSource(value = {
+            "'pobi,jason', 2",
+            "'pobi,jason,ever', 3",
+            "'pobi', 1",
+            "'  ', 1"
+    })
+    void nameSplit_success(String input, int nameCount) {
+        // when
+        List<String> names = CommaSplitter.split(input);
+
+        // then
+        assertEquals(nameCount, names.size());
     }
 }
