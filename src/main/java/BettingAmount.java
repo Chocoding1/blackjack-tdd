94324f1339eb3977eb1c39515763f3bd425721eb
Chocoding1 <galmeagi2@naver.com>
2026-04-16 19:39:52 +0900

    feat(BettingAmount): 배팅 금액 숫자 타입 검증 로직 작성

--- src/main/java/BettingAmount.java
+++ src/main/java/BettingAmount.java
@@ -1,15 +1,24 @@
 public class BettingAmount {
 
-    private final String amount;
+    private final int amount;
 
-    public BettingAmount(String amount) {
-        validateEmpty(amount);
+    public BettingAmount(String input) {
+        validateEmpty(input);
+        int amount = convertToInt(input);
         this.amount = amount;
     }
 
     private void validateEmpty(String amount) {
         if (amount.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 배팅 금액은 공백일 수 없습니다.");
+        }
+    }
+
+    private int convertToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 숫자 형태로 입력해야 합니다.");
         }
     }
 }
