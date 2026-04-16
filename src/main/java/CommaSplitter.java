ac28d91bff9780e8bfb789ebb3d4dcbcdc65463b
Chocoding1 <galmeagi2@naver.com>
2026-04-16 18:37:04 +0900

    refactor(User, CommaSplitter): 매직 변수 상수화

--- src/main/java/CommaSplitter.java
+++ src/main/java/CommaSplitter.java
@@ -3,16 +3,20 @@ import java.util.List;
 
 public class CommaSplitter {
 
+    private static final String COMMA = ",";
+
+    private static final String ERROR_EMPTY_INPUT = "[ERROR] 입력값은 공백일 수 없습니다.";
+
     public static List<String> split(String input) {
         validateEmptyInput(input);
 
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(COMMA))
                 .toList();
     }
 
     private static void validateEmptyInput(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 입력값은 공백일 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_EMPTY_INPUT);
         }
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,6 +2,9 @@ public class User {
 
     private static final String NAME_REGEX = "^[a-zA-Z가-힣]+$";
 
+    private static final String ERROR_EMPTY_NAME = "[ERROR] 사용자의 이름은 공백일 수 없습니다.";
+    private static final String ERROR_INVALID_NAME_REGEX = "[ERROR] 사용자의 이름은 영어 또는 한글로만 입력할 수 있습니다.(띄어쓰기 불가)";
+
     private final String name;
 
     public User(String name) {
@@ -12,13 +15,13 @@ public class User {
 
     private void validateEmpty(String name) {
         if (name.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 사용자의 이름은 공백일 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_EMPTY_NAME);
         }
     }
 
     private void validateInvalidFormat(String name) {
         if (!name.matches(NAME_REGEX)) {
-            throw new IllegalArgumentException("[ERROR] 사용자의 이름은 영어 또는 한글로만 입력할 수 있습니다.(띄어쓰기 불가)");
+            throw new IllegalArgumentException(ERROR_INVALID_NAME_REGEX);
         }
     }
 }
