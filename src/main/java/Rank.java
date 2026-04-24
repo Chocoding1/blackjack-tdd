d2978a3afd061d50ad3e0675b8d70e3493b4deff
Chocoding1 <galmeagi2@naver.com>
2026-04-24 18:15:34 +0900

    feat(Rank): 테스트에 정의한 Rank 객체들 추가

--- src/main/java/Rank.java
+++ src/main/java/Rank.java
@@ -1,5 +1,15 @@
 public enum Rank {
 
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    ;
+
     private final int score;
 
     Rank(int score) {
