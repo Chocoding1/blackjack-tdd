e298670bb6b4b57c15d392a39c94a25d41104e78
Chocoding1 <galmeagi2@naver.com>
2026-04-24 18:10:29 +0900

    test(RankTest): 2 ~ 9 Rank 도메인 규칙 정의 테스트 코드 작성

--- src/test/java/RankTest.java
+++ src/test/java/RankTest.java
@@ -1,3 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class RankTest {
 
+    @ParameterizedTest
+    @DisplayName("2 ~ 9 Rank는 각각 2 ~ 9의 점수를 반한다.")
+    @CsvSource({
+            "TWO, 2",
+            "THREE, 3",
+            "FOUR, 4",
+            "FIVE, 5",
+            "SIX, 6",
+            "SEVEN, 7",
+            "EIGHT, 8",
+            "NINE, 9"
+    })
+    void rankScoreTest(Rank rank, int expectedScore) {
+        // when & then
+        Assertions.assertEquals(expectedScore, rank.getScore());
+    }
 }
