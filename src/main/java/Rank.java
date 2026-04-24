4fb53557e95d6c7bef115d05b913efb1a23f439e
Chocoding1 <galmeagi2@naver.com>
2026-04-24 18:51:24 +0900

    feat(Rank): format 필드 추가

--- src/main/java/Rank.java
+++ src/main/java/Rank.java
@@ -1,23 +1,25 @@
 public enum Rank {
 
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    J(10),
-    Q(10),
-    K(10),
+    ACE(1, "1"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "$"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    J(10, "10"),
+    Q(10, "10"),
+    K(10, "10"),
     ;
 
     private final int score;
+    private final String format;
 
-    Rank(int score) {
+    Rank(int score, String format) {
         this.score = score;
+        this.format = format;
     }
 
     public int getScore() {
