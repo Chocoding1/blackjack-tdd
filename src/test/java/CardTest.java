9aed45165f0df55ccfda1ab78c30e7f50a8b893c
Chocoding1 <galmeagi2@naver.com>
2026-04-27 19:35:28 +0900

    test(CardTest): Card 생성 테스트 코드 작성

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,3 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CardTest {
 
+    @Test
+    @DisplayName("Card는 Rank와 Suit을 파라미터로 받아 생성된다.")
+    void createCard_success() {
+        // given
+        Card card = new Card(Rank.ACE, Suit.CLOVER);
+
+        // when & then
+        assertThat(card).isNotNull();
+    }
 }
